f864a8469337208a52a62b20c67c3377ec0fe7bd
aminamos <26092352+aminamos@users.noreply.github.com>
2020-02-05 17:10:00 -0800

    draft

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,21 @@
 export default (state = [], action) => {
-  return state;
+  let index;
+
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+    case "REMOVE_QUOTE":
+      index = state.findIndex(quote => quote.id === action.id);
+      return [...state.slice(0, index), ...state.slice(index + 1)];
+    case "UPVOTE_QUOTE":
+      index = state.findIndex(quote => quote.id === action.id);
+      let quote = state[index]
+      console.log(quote)
+      // quote.votes += 1
+      return [...state,]
+    case "DOWNVOTE_QUOTE":
+      return 'hi'
+    default:
+      return state;
+  }
 }
